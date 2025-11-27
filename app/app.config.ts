507b16bf8eaf7576a614f7081d06379af547babd
xiaoxiaoyaonie <51020961+xiaoxiaoyaonie@users.noreply.github.com>
2025-11-27 16:01:48 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,13 +1,13 @@
 export default defineAppConfig({
-  title: 'Sink',
-  email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/ccbikai/sink',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  title: '短链接测试',
+  email: '',
+  github: 'https://github.com/xiaoxiaoyaonie/sink2',
+  twitter: '',
+  telegram: '',
+  mastodon: '',
+  blog: '',
+  description: '',
+  image: '',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
